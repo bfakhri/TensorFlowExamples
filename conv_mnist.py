4810b0f98491f884d4bc0e48c7b6878d03cedd90
Bijan Fakhri (Student) <bfakhri@en4113734l.fulton.ad.asu.edu>
2017-09-30 16:06:55 -0700

    Added Structured Scopes

--- conv_mnist.py
+++ conv_mnist.py
@@ -15,11 +15,13 @@ def bias_variable(shape):
 
 
 # Define conv and pool functions
-def conv2d(x, W):
-      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, name='conv'):
+    with tf.name_scope(name):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def max_pool_2x2(x, name='max_pool'):
+    with tf.name_scope(name):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 # Get Data
 mnist = mnist_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -32,49 +34,52 @@ BATCH_SIZE = 512
 MAX_TRAIN_STEPS = 1000 
 output_steps = 20
 
-# Placeholders for data and labels
-x = tf.placeholder(tf.float32, shape=[None, SIZE_X*SIZE_Y])
-y_true = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
-# Dropout Placeholder (probability of dropping)
-keep_prob = tf.placeholder(tf.float32)
+with tf.name_scope('MainGraph'):
+    with tf.name_scope('Inputs'):
+        # Placeholders for data and labels
+        x = tf.placeholder(tf.float32, shape=[None, SIZE_X*SIZE_Y])
+        y_true = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
+        # Dropout Placeholder (probability of dropping)
+        keep_prob = tf.placeholder(tf.float32)
 
-# Reshape X to make it into a 2D image
-x_image = tf.reshape(x, [-1, SIZE_X, SIZE_Y, 1])
-
-# Weights 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-# Convolve and Maxpool layer 1
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# Convolve and Maxpool layer 2
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# FC Layer 1
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# Dropout Layer
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# FC Layer 2 - Output Layer
-y_pred = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        # Reshape X to make it into a 2D image
+        x_image = tf.reshape(x, [-1, SIZE_X, SIZE_Y, 1])
 
 
-# Objective Function
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
+    with tf.name_scope('Conv1'):
+        W_conv1 = weight_variable([5, 5, 1, 32])
+        b_conv1 = bias_variable([32])
+
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.name_scope('Conv2'):
+        W_conv2 = weight_variable([5, 5, 32, 64])
+        b_conv2 = bias_variable([64])
+
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = max_pool_2x2(h_conv2)
+
+
+    with tf.name_scope('FC1'):
+        W_fc1 = weight_variable([7 * 7 * 64, 1024])
+        b_fc1 = bias_variable([1024])
+        
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+    with tf.name_scope('FC1'):
+        W_fc2 = weight_variable([1024, 10])
+        b_fc2 = bias_variable([10])
+        # Dropout Layer
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+        # FC Layer 2 - Output Layer
+        y_pred = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+
+    with tf.name_scope('Objective'):
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
 
 # Define the training step
 train_step = tf.train.AdamOptimizer(LEARN_RATE).minimize(cross_entropy)
