ea4c47d22f1006b5b8b4be0b9e29ea0096e82c54
tubi <bfakhri@asu.edu>
2017-10-03 20:39:31 -0700

    Updated trainmonitor.sh

--- trainNmonitor.sh
+++ trainNmonitor.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
-tmux new-session -s 'train' -d 'python test.py'
+tmux new-session -s 'train' -d 'python conv_mnist.py'
 tmux split-window -v 'watch -n 0.1 nvidia-smi'
 tmux split-window -h 'htop'
 tmux rename-window 'Monitor'
-tmux new-window 'tensorboard --logdir=/tmp/tbdir/1 --port=12345'
+tmux new-window 'tensorboard --logdir=./logs/1 --port=12345'
 tmux rename-window 'Tensorboard'
 tmux select-window -t 0
 tmux -2 attach-session -d 
