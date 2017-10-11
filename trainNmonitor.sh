e1a3951e44dfa50a41181d26ca054b117f850e20
Bijan Fakhri (Student) <ASUAD\bfakhri@en4146051l.fulton.ad.asu.edu>
2017-10-11 13:31:31 -0700

    Trainmonitor now launches tensorboard pointed at WHOLE log dir

--- trainNmonitor.sh
+++ trainNmonitor.sh
@@ -4,7 +4,7 @@ tmux split-window -h 'watch -n 0.1 nvidia-smi'
 tmux split-window -v 'htop'
 tmux rename-window 'Monitor'
 tmux set -g remain-on-exit on
-tmux new-window 'tensorboard --logdir=./logs/1 --port=12345'
+tmux new-window 'tensorboard --logdir=./logs/ --port=12345'
 tmux rename-window 'Tensorboard'
 tmux select-window -t 0
 tmux -2 attach-session -d 
